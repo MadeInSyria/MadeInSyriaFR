9f443351eac2e22e2f1619ee70863844cac0353b
Omar Alaouf <omar@alaouf.com>
2013-12-29 19:20:02 +0100

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  #Testing home page
+  describe "Home page" do
+    it "should have the content 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Home')
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("MadeInSyria.fr | Home")
+    end
+  end
+
+  #Testing about page
+  describe "About page" do
+    it "should have the content 'About Me'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About me')
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("MadeInSyria.fr | About me")
+    end
+  end
+end
