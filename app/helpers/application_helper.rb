56a1262ee74c5e5043bc6a8bf8b3bb8d79da3503
Omar Alaouf <omar@alaouf.com>
2013-12-29 21:09:35 +0100

    Setting default page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "MadeInSyria.fr"
+    if page_title.empty?
+      "#{base_title} | For a Geek New World" 
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
